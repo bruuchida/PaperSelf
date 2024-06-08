065b50227f370d392de1e9e9c4b3bb9c18c870fd
Bruna Uchida <brunatiemiuchida@gmail.com>
2024-06-08 20:42:11 -0300

    fix: adjust about and steps pages

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
 var btn = document.getElementById("btnInicio");
 
-btn.addEventListener('scroll', scrollFunction())
-
-// window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    btn.style.display = "block";
-    } else {
-    btn.style.display = "none";
-    }
-}
-
-
-function voltaInicio() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0; 
+function toggleMenu() {
+    const menu = document.getElementById('menu-links')
+    menu.classList.toggle('header__nav--open')
 }
